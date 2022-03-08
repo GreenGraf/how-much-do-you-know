bd4935a6ff52424fc98b213a79643c04a391136b
GreenGraf <moleary27@gmail.com>
2022-03-08 16:01:23 +0000

    Add dynamic question counter

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 
 let currentQuestion = {}
@@ -56,6 +58,8 @@ startGame = () => {
 
 getNewQuestion = function () {
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
