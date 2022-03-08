cabf2ec80d8cf8ebb4284b5701091ad882efb301
GreenGraf <moleary27@gmail.com>
2022-03-08 21:28:29 +0000

    Add display high score from previous game

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,5 +1,10 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+
+// ENTER NAME AND SAVE
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
