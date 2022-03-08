4dac0d3fc8064281411084f0a4f05b6f29bff873
GreenGraf <moleary27@gmail.com>
2022-03-08 21:48:13 +0000

    Add local storage for high scores

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,13 +4,29 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
-// ENTER NAME AND SAVE
+// LOCAL STORAGE FOR SCORES
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+const MAX_HIGH_SCORES = 5;
+
+// ENTER NAME AND SAVE
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
+// LOCAL STORAGE FOR SCORES
 saveHighScore = e => {
-    console.log("clicked save");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+    
 }
