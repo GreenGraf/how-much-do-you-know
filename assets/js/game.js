774fdc94db51ecb1180242e4bb467f185dac9d0d
GreenGraf <moleary27@gmail.com>
2022-03-09 18:29:22 +0000

    Add code to make questions and event listener function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,10 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+let choices;
+let correctAnswers = [];
 const questionCounterText = document.getElementById('questionCounter');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const numToAlpha = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F'}
 
 
 let currentQuestion = {}
@@ -15,7 +17,7 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("https://quizapi.io/api/v1/questions?apiKey=9Si0Eb5bu9YIwFFSwLG3E4Fmo5pkvxSfmATADrph&limit=10&category=Linux&difficulty=easy").then( res => { 
+fetch("https://quizapi.io/api/v1/questions?apiKey=9Si0Eb5bu9YIwFFSwLG3E4Fmo5pkvxSfmATADrph&limit=10&category=Linux&difficulty=hard").then( res => { 
     return res.json();
     })
     .then(loadedQuestions => {
@@ -41,13 +43,19 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 }
 
 // populate choices and assign a number to them
 
 getNewQuestion = function () {
+
+    // Remove Old Questions
+    document.getElementById('choices-wrapper').innerHTML = '';
+    // Reset Correct Answers
+    correctAnswers = [];
+    
+
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
@@ -61,45 +69,148 @@ getNewQuestion = function () {
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
+
+    if (currentQuestion.correct_answer) {
+        correctAnswers.push(currentQuestion.correct_answer);
+    }
+
+    
+    let questionNumber = 1;
+
+    // Generate New Questions
+
+    console.log(currentQuestion)
+
+    for (let answer_key in currentQuestion.answers) {
+
+        
+
+
+        if (currentQuestion.answers[answer_key]) {
+
+            let newAnswer = currentQuestion.answers[answer_key];
+
+            let newChoice = `
+
+                <div class="choice-container">
+                    <p class="choice-prefix">${numToAlpha[questionNumber]}</p>
+                    <p class="choice-text" data-value="answer_${numToAlpha[questionNumber].toLowerCase()}">${newAnswer}</p>
+                </div>
+
+                `
+            document.getElementById('choices-wrapper').innerHTML += newChoice;
+            questionNumber++;
+        }
+    }
+
+    // Obtain Correct Answers
+
+
+
+
+
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
-        const number = choice.dataset['value'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
+    choices = Array.from(document.getElementsByClassName("choice-text"))
+
 
     availableQuestions.splice(questionIndex, 1);
 
     acceptingAnswers = true;
-};
+
+
+    for (let answer_key in currentQuestion.correct_answers) {
+        if (currentQuestion.correct_answers[answer_key] === "true") {
+            correctAnswers.push(answer_key.slice(0, 8))
+        }
+    }
+
+    console.log(correctAnswers)
+
+    // *******
+
+    choices.forEach(choice => {
+
+
+
+        const answerValue = choice.dataset['value'];
+        
+
+        choice.addEventListener('click', e => {
+            if(!acceptingAnswers) return;
+            
+            // Check the button value to see if it's in correct answer
+
+            const selectedChoice = e.target;
+            const selectedAnswer = selectedChoice.dataset["value"];
+
+            console.log(selectedChoice)
+
+            let classToApply;
+
+            console.log(correctAnswers)
+            console.log("---")
+            console.log(selectedAnswer)
+            console.log("---")
+
+            if (correctAnswers.includes(selectedAnswer)) {
+                classToApply = "correct";
+            } else {
+                classToApply = "incorrect"
+            }
+
+            if(classToApply === 'correct'){
+                incrementScore(CORRECT_BONUS);
+            }
+
+            selectedChoice.parentElement.classList.add(classToApply);
+
+            setTimeout( () => {
+                selectedChoice.parentElement.classList.remove(classToApply);
+                getNewQuestion();
+            }, 1000);    
+
+
+            acceptingAnswers = false;
+        
+        });
+    });
+
+};    // ******
+
+
 
 // Check to see if answer is correct and move to next question
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+// choices.forEach(choice => {
+//     choice.addEventListener('click', e => {
+//         if(!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["value"];
+//         acceptingAnswers = false;
+//         const selectedChoice = e.target;
+//         const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";    
+//         const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";    
 
-        if(classToApply === 'correct'){
-            incrementScore(CORRECT_BONUS);
-        }
+//         if(classToApply === 'correct'){
+//             incrementScore(CORRECT_BONUS);
+//         }
 
-        selectedChoice.parentElement.classList.add(classToApply);
+//         selectedChoice.parentElement.classList.add(classToApply);
 
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);    
-    });
-})
+//         setTimeout( () => {
+//             selectedChoice.parentElement.classList.remove(classToApply);
+//             getNewQuestion();
+//         }, 1000);    
+//     });
+// })
+
+
+
+
+    
 
 incrementScore = num => {
     score +=num;
     scoreText.innerText = score;
 }
-
